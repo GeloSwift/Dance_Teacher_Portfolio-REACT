e1dfd8763de7b066f5093a4f747e0cf52b456840
GeloSwift <angelo.nichele317@gmail.com>
2025-08-04 00:15:30 +0200

    feat: initialize React app with routing and Tailwind CSS

---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+export const Home = () => {
+    return <div className="min-h-screen">
+
+        {/* Theme Toggle */}
+
+
+        {/* Background effects */}
+
+
+        {/* Navbar */}
+
+
+        {/* Main Content */}
+
+
+        {/* Footer */}
+
+    </div>;
+};
